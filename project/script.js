d0553026b7449a8ea9f04d26e59d542a44bd4c06
mickeykhs <karjablee1@gmail.com>
2022-08-17 08:42:52 +0900

    second commit

--- project/script.js
+++ project/script.js
@@ -14,7 +14,20 @@ let backgroundImage,spaceshipImage,bulletImage,enemyImage,gameOverImage;
 let spaceshipX = canvas.width/2 - 30;
 let spaceshipY = canvas.height-60;
 
+let bulletList = []; // 총알들을 저장하는 리스트
+function Bullet(){
+    this.x=0;
+    this.y=0;
+    this.init=function(){
+        this.x=spaceshipX+19;
+        this.y=spaceshipY;
 
+        bulletList.push(this);
+    };
+    this.update = function(){
+        this.y -= 7;
+    };
+}
 function loadImage(){
     backgroundImage = new Image();
     backgroundImage.src = "images/background.gif";
@@ -35,13 +48,25 @@ function loadImage(){
 let keysDown={};
 function setupKeyboardListener(){
     document.addEventListener("keydown", function(event){
+
+
         keysDown[event.keyCode] = true;
-        console.log("키다운 객체에 들어간 값은 ?" , keysDown);
+        // console.log("키다운 객체에 들어간 값은 ?" , keysDown);
     });
     document.addEventListener("keyup", function(event){
         delete keysDown[event.keyCode];
-        console.log("버튼클릭후", keysDown);
+
+        if(event.keyCode == 32){
+            createBullet(); // 총알 생성
+        }
+        // console.log("버튼클릭후", keysDown);
     });
+}
+
+function createBullet(){
+    let b = new Bullet();
+    b.init();
+    console.log("새로운 총알 리스트", bulletList);
 }
 
 function update(){
@@ -59,11 +84,20 @@ function update(){
     if(spaceshipX >=canvas.width-60){
         spaceshipX = canvas.width-60;
     }
+
+    // 총알의 y좌표 업데이트 하는 함수 포출
+    for(let i=0; i<bulletList.length; i++){
+        bulletList[i].update();
+    }
 }
 
 function render(){
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(spaceshipImage,spaceshipX,spaceshipY);
+
+    for(let i=0; i<bulletList.length; i++){
+        ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y);
+    }
 }
 
 function main(){
@@ -80,3 +114,9 @@ main();
 // 우주선의 xy 좌표가 바뀌고
 // 다시 render를 그려준다.
 
+// 총알 만들기
+// 1. 스페이스바를 누르면 총알 발사
+// 2. 총알이 발사 = 총알의 y값이 -- , 총알의 x값은? 스페이스를 누른 순간의 우주선의 x좌표
+// 3. 발사된 총알들은 총알배열에 저장을 한다
+// 4. 모든 총알들은 x,y좌표값이 있어야 한다
+// 5. 총알 배열을 가지고 render 그려준다
