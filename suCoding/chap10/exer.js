98eab50e10684f87f99c08f4dfb26cfa5f5b4dd6
mickeykhs <karjablee1@gmail.com>
2024-03-27 13:12:29 +0900

    c7 commit

--- suCoding/chap10/exer.js
+++ suCoding/chap10/exer.js
@@ -11,10 +11,6 @@ console.log(`원의 넓이 : ${area}`);
 /* 매개변수로 배열을 전달받아 배열의 요소 중에서 가장 큰 수를 찾아 반환하는 함수를 만들어 보세요. 
 이때, 배열안의 데이터는 모두 0보다 큰 정수라고 가정합니다. */
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 51ccf5c61f7819b71055619850987fb522f911aa
 function getOrderDesc(arr){
     let result = 0;
     for(let i=0; i<arr.length; i++){
